3a0cab084ff09cf3642d5b22b3741d343b9f03d9
Alexandre Ferreira <shieldforce2@gmail.com>
2020-12-25 17:21:40 -0300

    error trait not found

--- src/AutoValidation/Observers/InterceptObserversModel.php
+++ src/AutoValidation/Observers/InterceptObserversModel.php
@@ -2,11 +2,13 @@
 
     namespace ShieldForce\AutoValidation\Observers;
 
+    use Illuminate\Database\Eloquent\Model;
+
     class InterceptObserversModel
     {
-        public function __construct()
+        public function __construct(Model $model)
         {
-            dd("observers");
+            dd($model);
         }
 
     }
